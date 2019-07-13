c08281ba1da27d087964c3ca3ab8804369a4c508
skhuteta <skhuteta@DIN73002996.corp.capgemini.com>
2019-07-13 14:01:12 +0530

    sadad

--- ModuleOne_JDBC/src/com/bank/exception/LowBalanceException.java
+++ ModuleOne_JDBC/src/com/bank/exception/LowBalanceException.java
@@ -2,6 +2,11 @@ package com.bank.exception;
 
 public class LowBalanceException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	public String toString() {
 		return "Insufficient Balance in your account";
